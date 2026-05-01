04dcb3a46cec9886bd2ddfc8a6e66558852401bd
catbiscuits21 <68904460+catbiscuits21@users.noreply.github.com>
2026-05-01 23:20:32 +0930

    Media Preview Modal

---
+++ index.js
@@ -0,0 +1,30 @@
+//opens the modal for previewing media. Makes the modal's content include the src and creates html for the given type of media
+//valid type parameters: video, image
+function openMediaPreview(src, type){
+    if (type !== "video" && type !== "image") { return; }
+    var modal = document.querySelector("#preview-modal");
+    var modalContent = document.querySelector("#preview-content");
+
+    switch (type){
+        case "video":{
+            modalContent.innerHTML = `
+                <video controls autoplay>
+                    <source src="` + src + `" type="video/mp4">
+                </video>`;
+            break;
+        }
+        case "image":{
+            modalContent.innerHTML = `<img src="` + src + `">`;
+            break;
+        }
+    }
+
+    modal.classList.add("open");
+}
+
+function closeMediaPreview(){
+    var modal = document.querySelector("#preview-modal");
+    var modalContent = document.querySelector("#preview-content");
+    modalContent.innerHTML = "";
+    modal.classList.remove("open");
+}
