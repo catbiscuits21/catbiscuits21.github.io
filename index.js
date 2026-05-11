10440ad90d0494576d9429fe3b528eb18fb20865
catbiscuits21 <68904460+catbiscuits21@users.noreply.github.com>
2026-05-12 00:44:36 +0930

    Play In Viewport
    Only plays media when in viewport. This is more 'alive' then playing when hovered. This also looks alot better on mobile.

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-const projectMediaContainers = document.querySelectorAll(".project-media-container");
+playInViewport();
 
-projectMediaContainers.forEach((container) => {
-  const video = container.querySelector("video");
+//this enables the feature where videos in project media containers play when they are hovered
+function playOnHover(){
+    const containers = document.querySelectorAll(".project-media-container");
+    containers.forEach((container) => {
+    const video = container.querySelector("video");
 
-  if (video != null) {
-    container.addEventListener("mouseenter", () => 
-        {
-            video.play(); 
-        });
+    if (video != null) {
+        container.addEventListener("mouseenter", () => 
+            {
+                video.play(); 
+            });
 
-    container.addEventListener("mouseleave", () => 
-        {
-            video.pause();
-            video.currentTime = 0;
-        });
-  }
-});
+        container.addEventListener("mouseleave", () => 
+            {
+                video.pause();
+                video.currentTime = 0;
+            });
+    }
+    });
+}
+
+//this enables the feature where videos in project media containers autoplay when they are in the viewport
+function playInViewport(){
+    const containers = document.querySelectorAll(".project-media-container");
+    const videos = [];
+
+    containers.forEach((container) => {
+        const video = container.querySelector("video");
+
+        if (video != null) {
+            videos.push(video);
+        }
+    })
+
+    const observerOptions = {
+        threshold: 0.5 //this determines how much of the element must be seen before it can be determined as fully observed
+    };
+
+    //add a event listener for an observer invoke
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            const video = entry.target;
+            if (entry.isIntersecting){
+                video.play();
+                console.log("Trying to play video");
+            }
+            else{
+                console.log("Pausing video");
+                video.pause();
+                video.currentTime = 0;
+            }
+        })
+    }, observerOptions); 
+
+    //invoke observation for every project media container
+    videos.forEach((video) => {
+        observer.observe(video);
+    }); 
+}
 
 //opens the modal for previewing media. Makes the modal's content include the src and creates html for the given type of media
 //valid type parameters: video, image
@@ -27,8 +70,8 @@ function openMediaPreview(src, type){
     switch (type){
         case "video":{
             modalContent.innerHTML = `
-                <video controls autoplay>
-                    <source src="` + src + `" type="video/mp4">
+                <video controls autoplay inline>
+                    <source src="` + src + `" type="video/webm">
                 </video>`;
             break;
         }
